06576d107154dbb30e37ab44345a8f01ae2a9226
Gabriel Acosta Belchior <gabriel.acostab@hotmail.com>
2019-03-21 18:12:45 -0300

    Inicio do Login

--- App/src/index.js
+++ App/src/index.js
@@ -3,15 +3,20 @@ import App from './App.vue';
 import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import Layout from "./components/Layout.vue"
 import Home from "./components/Home.vue"
 import User from "./components/user/User.vue"
 import userCreate from "./components/user/User.Create.vue"
+import Login from "./components/Login.vue"
 
+Vue.use(Layout)
 Vue.use(VueRouter)
 Vue.use(Vuetify)
+//Vue.use(Login)
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/login', component: Login },
     { path: '/user', component: User },
     { path: '/user/create', component: userCreate },
     { path: '*', redirect: '/' }
@@ -25,4 +30,7 @@ const app = new Vue({
     el: '#app',
     render: h => h(App),
     router: router,
+    components: {
+        Login: Login
+    }
 })
